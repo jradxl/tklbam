1e11101f0b997c1821b68d9f24576f063ef964ba
Liraz Siri <liraz@sterilesecurity.com>
2010-08-29 15:07:51 +0300

    restore_rollback => restore-rollback

--- cmd.py
+++ cmd.py
@@ -23,7 +23,7 @@ class CliWrapper(CliWrapper):
                             '',
                             'passphrase', 'escrow',
                             '',
-                            'backup', 'list', 'restore', 'restore_rollback']
+                            'backup', 'list', 'restore', 'restore-rollback']
 
 if __name__ == "__main__":
     CliWrapper.main()
